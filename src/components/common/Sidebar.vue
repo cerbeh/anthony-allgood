e8796bd5b76153e0b80bc7746dc69c4c0a81641f
Martin Allgood <martintallgood@gmail.com>
2018-09-07 13:53:10 +0100

    Added component to list each feature from the original 3 sidebar items

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -2,13 +2,13 @@
   <div class="sidebar-style">
     <ul class="navbar-style" v-if="sidebarOn">
       <li>
-        <a href="/#/about" class="sidebar-item">About</a>
+        <AboutList />
       </li>
       <li>
-        <a href="/#/gallery" class="sidebar-item">Gallery</a>
+        <a href="/#/gallery" class="sidebar-item" v-on:click="toggleSidebar">Gallery</a>
       </li>
       <li>
-        <a href="/#/contact" class="sidebar-item">Contact</a>
+        <a href="/#/contact" class="sidebar-item" v-on:click="toggleSidebar">Contact</a>
       </li>
     </ul>
     <i class="fas fa-chevron-right fa-3x sidebar-item" v-on:click="toggleSidebar" v-if="!sidebarOn"></i>
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+import AboutList from '../about/list';
 export default {
   name: 'sidebar',
   data() {
@@ -28,6 +29,9 @@ export default {
     toggleSidebar() {
       this.sidebarOn = !this.sidebarOn;
     }
+  },
+  components: {
+    AboutList
   }
 }
 </script>
@@ -36,7 +40,6 @@ export default {
 .sidebar-style {
   height: 100vh;
   display: flex;
-  /* position: absolute; */
 }
 .navbar-style {
   display: flex;
@@ -44,14 +47,15 @@ export default {
   align-items: center;
   height: inherit;
   justify-content: center;
-  left: -400px;
-  /* margin: 20 0 30 0; */
-  /* margin-left: 20%; */
+  animation: toggleSidebarOn .05s 1;
 }
-.sidebar-item {
-  padding: 20px;
-  margin: 10px;
-  display: flex;
-  align-items: center;
+
+@keyframes toggleSidebarOn {
+  0% {
+    transform:translateX(-150px);
+  }
+  100% {
+    transform:translateX(0);
+  }
 }
 </style>
