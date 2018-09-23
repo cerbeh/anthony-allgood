27d2ef985300be3281471e2e7beb2d6891b87351
Martin Allgood <martintallgood@gmail.com>
2018-09-23 17:15:03 +0100

    Added container div to working credits page

--- src/components/workingcredits/WorkingCreditsShowCard.vue
+++ src/components/workingcredits/WorkingCreditsShowCard.vue
@@ -1,24 +1,26 @@
 <template lang="html">
-  <section>
-    <h1 class="title is-2">Working Credits: {{credits.discipline}}</h1>
-    <table class="table is-striped is-hoverable is-fullwidth">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Character</th>
-          <th>Director</th>
-          <th>Company</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="credit in credits.credits">
-          <td>{{credit.title}}</td>
-          <td>{{credit.character}}</td>
-          <td>{{credit.director}}</td>
-          <td>{{credit.company}}</td>
-        </tr>
-      </tbody>
-    </table>
+  <section class="section">
+    <div class="container">
+      <h1 class="title is-2">Working Credits: {{credits.discipline}}</h1>
+      <table class="table is-striped is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Character</th>
+            <th>Director</th>
+            <th>Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="credit in credits.credits">
+            <td>{{credit.title}}</td>
+            <td>{{credit.character}}</td>
+            <td>{{credit.director}}</td>
+            <td>{{credit.company}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
